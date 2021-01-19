79f151f1679abf8ef0aa6c82de11af400733165a
Nelio Alves <joaopedromonteiro2014@gmail.com>
2021-01-19 20:22:23 -0300

    endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -5,10 +5,13 @@ import java.io.Serializable;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 public class ItemPedido implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPk id = new ItemPedidoPk();	
 	private Double desconto;
@@ -30,9 +33,11 @@ public class ItemPedido implements Serializable{
 	}
 	
 	//get e set
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
+	
 	public Produto getProduto() {
 		return id.getProduto();
 	}	
